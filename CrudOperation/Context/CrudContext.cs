80c4dd6e19c380b3523bcb1b402e9d25c841fda1
Ashibur Rahman Munna <jibonrahmanctg@gmail.com>
2023-04-24 20:06:52 +0600

    Add project files.

---
+++ CrudOperation/Context/CrudContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudOperation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperation.Context
+{
+    public class CrudContext : DbContext
+    {
+        public CrudContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee>Employee { get; set; }
+    }
+}
---
+++ CrudOperation/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudOperation.Models
+{
+    public class Employee
+    {
+        [Display(Name = "Employee ID")]
+        public int Id { get; set; }
+
+        [Display(Name = "Employee Name")]
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public long Salary { get; set; }
+    }
+}
