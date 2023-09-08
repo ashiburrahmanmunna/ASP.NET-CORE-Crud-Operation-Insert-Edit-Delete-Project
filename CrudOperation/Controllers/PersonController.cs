e030f7ae45669b5afcde771d98175ce8ae6600a6
Ashibur Rahman Munna <jibonrahmanctg@gmail.com>
2023-09-08 23:18:49 +0600

    done

--- CrudOperation/Controllers/PersonController.cs
+++ CrudOperation/Controllers/PersonController.cs
@@ -16,15 +16,29 @@ namespace CrudOperation.Controllers
             _context = context;
         }
         // GET: Person
-        public ActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return _context.Person != null ?
+                        View(await _context.Person.ToListAsync()) :
+                        Problem("Entity set 'CrudContext.Employee'  is null.");
         }
 
         // GET: Person/Details/5
-        public ActionResult Details(int id)
+        public async Task<IActionResult> Details(int? id)
         {
-            return View();
+            if (id == null || _context.Person == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Person
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
         }
 
         // GET: Person/Create
@@ -76,24 +90,39 @@ namespace CrudOperation.Controllers
         }
 
         // GET: Person/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int? id)
         {
-            return View();
+            if (id == null || _context.Person == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Person
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
         }
 
         // POST: Person/Delete/5
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> DeletePost(Person person)
         {
-            try
+
+            ViewBag.Title = "Delete";
+            var obj = _context.Person.Find(person.Id);
+            if (ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Index));
+                _context.Remove(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Person");
             }
-            catch
-            {
-                return View();
-            }
+            return View(obj);
+
         }
     }
 }
