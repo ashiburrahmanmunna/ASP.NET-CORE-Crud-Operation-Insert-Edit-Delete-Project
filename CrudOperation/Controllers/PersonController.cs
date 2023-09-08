7c65a0a9c9a8b11f307a6058827e6d5c162f1dbf
Ashibur Rahman Munna <jibonrahmanctg@gmail.com>
2023-09-08 17:04:56 +0600

    done

---
+++ CrudOperation/Controllers/PersonController.cs
@@ -0,0 +1,99 @@
+﻿using CrudOperation.Context;
+using CrudOperation.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CrudOperation.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly CrudContext _context;
+
+        public PersonController(CrudContext context)
+        {
+            _context = context;
+        }
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Person/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Person/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Person person)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(person);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(person);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Person/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Person/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
